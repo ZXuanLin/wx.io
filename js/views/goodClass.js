158ce7d774531dc1a1f1c79c8145f16a889c570c
ZXuanLin <794648899@qq.com>
2017-06-06 11:48:20 +0800

    first

---
+++ js/views/goodClass.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    function tabGo() {
+        var p_cjs = $('.p-cjs');
+        p_cjs.on('touchstart', 'li', function () {
+            $('.p-cjs li a').removeClass('current')
+            $(this).find('a').addClass('current');
+        })
+    }
+    tabGo();
+    createPage.returnPage('createGoodClass', 'wwwwwwwww')
+})
